54cf3c62ba81b70d066d9f0d810eaa672e0b5de9
guilherme chaves <chaves.guilherme@proton.me>
2025-11-14 20:57:22 -0300

    chore(todo): adiciona componente Text

--- 2-todo/src/App.tsx
+++ 2-todo/src/App.tsx
@@ -1,5 +1,18 @@
+import Text from "./components/Text";
+
 function App() {
-  return <div className="text-pink-base">Olá mundo</div>;
+  return (
+    <div className="flex flex-col gap-2">
+      <Text variant="body-sm-bold" className="text-pink-base">
+        Olá mundo
+      </Text>
+      <Text variant="body-md" className="text-green-base">
+        Olá mundo
+      </Text>
+      <Text variant="body-md-bold">Olá mundo</Text>
+      <Text>Olá mundo</Text>
+    </div>
+  );
 }
 
 export default App;
