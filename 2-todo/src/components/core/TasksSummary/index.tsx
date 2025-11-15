a30b70b2ed6559e22ad57d288a432f59156296c9
guilherme chaves <chaves.guilherme@proton.me>
2025-11-15 15:56:31 -0300

    fix(todo): adiciona componente TasksSummary

---
+++ 2-todo/src/components/core/TasksSummary/index.tsx
@@ -0,0 +1,20 @@
+import Badge from '../../ui/Badge';
+import Text from '../../ui/Text'
+
+function TasksSummary() {
+  return (
+    <>
+      <div className="flex items-center gap-2">
+        <Text variant="body-sm-bold" className="!text-gray-300">Tarefas criadas</Text>
+        <Badge variant="secondary">5</Badge>
+      </div>
+      <div className="flex items-center gap-2">
+        <Text variant="body-sm-bold" className="!text-gray-300">Concluídas</Text>
+        <Badge variant="primary">2 de 5</Badge>
+      </div>
+
+    </>
+  )
+}
+
+export default TasksSummary;
--- 2-todo/src/pages/Home/index.tsx
+++ 2-todo/src/pages/Home/index.tsx
@@ -1,7 +1,13 @@
+import TasksSummary from "../../components/core/TasksSummary";
 import Container from "../../components/ui/Container";
 
 function HomePage() {
-  return <Container>PAGINA HOME</Container>;
+  return (
+    <Container as="article" className="space-y-3">
+      <header className="flex items-center justify-between">
+        <TasksSummary />
+      </header>
+    </Container>);
 }
 
 export default HomePage;
