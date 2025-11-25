e1831c53bbb55c1b7ae317c143b144a8ce47c6f5
guilherme chaves <chaves.guilherme@proton.me>
2025-11-25 20:52:06 -0300

    chore(gallery): adiciona axios e react query

--- 5-gallery-plus/src/App.tsx
+++ 5-gallery-plus/src/App.tsx
@@ -1,19 +1,24 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Route, Routes } from "react-router";
 import LayoutMain from "./pages/layout-main";
 import PageComponents from "./pages/page-components";
 import HomePage from "./pages/page-home";
 import PhotoDetailsPage from "./pages/page-photo-details";
 
+const queryClient = new QueryClient();
+
 export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route element={<LayoutMain />}>
-          <Route index element={<HomePage />} />
-          <Route path="/fotos/:id" element={<PhotoDetailsPage />} />
-          <Route path="/components" element={<PageComponents />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<LayoutMain />}>
+            <Route index element={<HomePage />} />
+            <Route path="/fotos/:id" element={<PhotoDetailsPage />} />
+            <Route path="/components" element={<PageComponents />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
   );
 }
