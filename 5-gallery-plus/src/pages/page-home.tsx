ebd3cb51de564edf35a11459a8a726988816e5c3
guilherme chaves <chaves.guilherme@proton.me>
2025-11-24 21:43:18 -0300

    chore(gallery): adiciona lista de albums

--- 5-gallery-plus/src/pages/page-home.tsx
+++ 5-gallery-plus/src/pages/page-home.tsx
@@ -1,9 +1,18 @@
 import Container from "../components/container";
+import AlbumsFilter from "../contexts/albums/components/albums-filter";
 import PhotoList from "../contexts/photos/components/photo-list";
 
 export default function HomePage() {
   return (
     <Container>
+      <AlbumsFilter
+        albums={[
+          { id: "123", title: "Album 1" },
+          { id: "321", title: "Album 2" },
+          { id: "333", title: "Album 3" },
+        ]}
+        className="mb-9"
+      />
       <PhotoList
         photos={[
           {
